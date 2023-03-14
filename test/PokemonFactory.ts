dc15f1a34174f00fd61439aff5338f18e9b6ced4
Rafael Livise <r.livise@hotmail.com>
2023-03-14 14:13:57 -0500

    feat: reto #1 event

--- test/PokemonFactory.ts
+++ test/PokemonFactory.ts
@@ -19,7 +19,7 @@ describe("PokemonFactory", function () {
     })
     it("Should to be array with one pokemon", async function () {
       const { pokemonFactory } = await loadFixture(deployPokemonFactory)
-      await pokemonFactory.createPokemon("bullbasur", 1)
+      await pokemonFactory.createPokemon(1, "bullbasur")
       const pokemons = await pokemonFactory.getAllPokemons()
       expect(Array.isArray(pokemons)).to.be.true;
       expect(pokemons.length).to.be.equal(1);
@@ -28,9 +28,21 @@ describe("PokemonFactory", function () {
 
     it("Should create first pokemon like a name bullbasur", async function () {
       const { pokemonFactory } = await loadFixture(deployPokemonFactory)
-      await pokemonFactory.createPokemon("bullbasur", 1)
+      await pokemonFactory.createPokemon(1, "bullbasur")
       const pokemons = await pokemonFactory.getAllPokemons()
       expect(pokemons[0].name).to.be.equal("bullbasur");
     })
   })
+
+  describe("event", async function () {
+    it("Should emit a event newPokemon", async function () {
+      const { pokemonFactory } = await loadFixture(deployPokemonFactory)
+      const pokemonName = "bullbasur";
+      const pokemonId = 1;
+      expect(pokemonFactory.createPokemon(pokemonId, pokemonName))
+        .to.emit(pokemonFactory, "eventNewPokemon")
+        .withArgs(pokemonId, pokemonName)
+    })
+  })
+
 })
